5e4244b19546445b161ae04250d6501b739b770d
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-04-01 16:52:22 -0400

    feat: list group role permissions

--- src/utils/role.utils.ts
+++ src/utils/role.utils.ts
@@ -1,21 +1,51 @@
 import { Namespace, TFunction } from "react-i18next";
-import { ServerPermissions } from "../constants/role.constants";
+import {
+  GroupPermissions,
+  ServerPermissions,
+} from "../constants/role.constants";
 
 export const getPermissionText = (
   name: string,
   t: TFunction<Namespace<"ns1">, undefined>
 ) => {
   switch (name) {
+    case GroupPermissions.AcceptMemberRequests:
+      return {
+        name: t("permissions.names.acceptMemberRequests"),
+        description: t("permissions.descriptions.acceptMemberRequests"),
+      };
+    case GroupPermissions.EditGroup:
+      return {
+        name: t("permissions.names.editGroup"),
+        description: t("permissions.descriptions.editGroup"),
+      };
+    case GroupPermissions.DeleteGroup:
+      return {
+        name: t("permissions.names.deleteGroup"),
+        description: t("permissions.descriptions.deleteGroup"),
+      };
+    case GroupPermissions.ManageSettings:
+      return {
+        name: t("permissions.names.manageSettings"),
+        description: t("permissions.descriptions.manageGroupSettings"),
+      };
+    case GroupPermissions.CreateEvents:
+      return {
+        name: t("permissions.names.createEvents"),
+        description: t("permissions.descriptions.createEvents"),
+      };
     case ServerPermissions.CreateInvites:
       return {
         name: t("permissions.names.createInvites"),
         description: t("permissions.descriptions.createInvites"),
       };
+    case GroupPermissions.ManageComments:
     case ServerPermissions.ManageComments:
       return {
         name: t("permissions.names.manageComments"),
         description: t("permissions.descriptions.manageComments"),
       };
+    case GroupPermissions.ManageEvents:
     case ServerPermissions.ManageEvents:
       return {
         name: t("permissions.names.manageEvents"),
@@ -26,16 +56,19 @@ export const getPermissionText = (
         name: t("permissions.names.manageInvites"),
         description: t("permissions.descriptions.manageInvites"),
       };
+    case GroupPermissions.ManagePosts:
     case ServerPermissions.ManagePosts:
       return {
         name: t("permissions.names.managePosts"),
         description: t("permissions.descriptions.managePosts"),
       };
+    case GroupPermissions.ManageRoles:
     case ServerPermissions.ManageRoles:
       return {
         name: t("permissions.names.manageRoles"),
         description: t("permissions.descriptions.manageRoles"),
       };
+    case GroupPermissions.BanMembers:
     case ServerPermissions.BanMembers:
       return {
         name: t("permissions.names.banMembers"),
