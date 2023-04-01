03e6f6fbe9e4118412a2de824e19f21101a275bc
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-04-01 15:11:59 -0400

    refactor: rename group roles page file

--- src/pages/groups/[name]/roles/index.tsx
+++ src/pages/groups/[name]/roles/index.tsx
@@ -4,15 +4,15 @@ import { NextPage } from "next";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
-import { breadcrumbsVar } from "../../../apollo/cache";
-import { useGroupRolesQuery } from "../../../apollo/gen";
-import RoleForm from "../../../components/Roles/RoleForm";
-import RoleList from "../../../components/Roles/RoleList";
-import ProgressBar from "../../../components/Shared/ProgressBar";
-import { TruncationSizes } from "../../../constants/common.constants";
-import { useIsDesktop } from "../../../hooks/common.hooks";
-import { isDeniedAccess } from "../../../utils/error.utils";
-import { getGroupPath } from "../../../utils/group.utils";
+import { breadcrumbsVar } from "../../../../apollo/cache";
+import { useGroupRolesQuery } from "../../../../apollo/gen";
+import RoleForm from "../../../../components/Roles/RoleForm";
+import RoleList from "../../../../components/Roles/RoleList";
+import ProgressBar from "../../../../components/Shared/ProgressBar";
+import { TruncationSizes } from "../../../../constants/common.constants";
+import { useIsDesktop } from "../../../../hooks/common.hooks";
+import { isDeniedAccess } from "../../../../utils/error.utils";
+import { getGroupPath } from "../../../../utils/group.utils";
 
 const GroupRoles: NextPage = () => {
   const { query, asPath } = useRouter();
