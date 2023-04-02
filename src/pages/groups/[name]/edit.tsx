c339c8510101f0f9bee8a412235d0add28e852c7
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-04-02 13:48:35 -0400

    feat: show breadcrumbs on edit group page

--- src/pages/groups/[name]/edit.tsx
+++ src/pages/groups/[name]/edit.tsx
@@ -1,12 +1,17 @@
 // TODO: Ensure EditGroup page is unreachable without role
 
 import { Typography } from "@mui/material";
+import { truncate } from "lodash";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
 import { useTranslation } from "react-i18next";
 import { useEditGroupQuery } from "../../../apollo/gen";
 import GroupForm from "../../../components/Groups/GroupForm";
+import Breadcrumbs from "../../../components/Shared/Breadcrumbs";
 import ProgressBar from "../../../components/Shared/ProgressBar";
+import { TruncationSizes } from "../../../constants/common.constants";
+import { useIsDesktop } from "../../../hooks/common.hooks";
+import { getGroupPath } from "../../../utils/group.utils";
 
 const EditGroup: NextPage = () => {
   const { query } = useRouter();
@@ -17,6 +22,7 @@ const EditGroup: NextPage = () => {
   });
 
   const { t } = useTranslation();
+  const isDesktop = useIsDesktop();
 
   if (error) {
     return <Typography>{t("errors.somethingWentWrong")}</Typography>;
@@ -30,7 +36,25 @@ const EditGroup: NextPage = () => {
     return null;
   }
 
-  return <GroupForm editGroup={data.group} />;
+  const breadcrumbs = [
+    {
+      label: truncate(name, {
+        length: isDesktop ? TruncationSizes.Small : TruncationSizes.ExtraSmall,
+      }),
+      href: getGroupPath(name),
+    },
+    {
+      label: t("groups.actions.edit"),
+    },
+  ];
+
+  return (
+    <>
+      <Breadcrumbs breadcrumbs={breadcrumbs} />
+
+      <GroupForm editGroup={data.group} />
+    </>
+  );
 };
 
 export default EditGroup;
