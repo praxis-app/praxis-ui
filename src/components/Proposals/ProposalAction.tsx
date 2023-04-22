76087bebdfa0866a0d3eb3a74dcb469d2810d753
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-04-22 10:40:39 -0400

    feat: add permissions and members for proposed role case in ProposalAction component

--- src/components/Proposals/ProposalAction.tsx
+++ src/components/Proposals/ProposalAction.tsx
@@ -1,8 +1,13 @@
-import { Box, Typography } from "@mui/material";
+import { Circle } from "@mui/icons-material";
+import { Box, Divider, Typography } from "@mui/material";
 import { useTranslation } from "react-i18next";
 import { ProposalActionFragment } from "../../apollo/gen";
 import { ProposalActionTypes } from "../../constants/proposal.constants";
+import { getUserProfilePath } from "../../utils/user.utils";
 import AttachedImage from "../Images/AttachedImage";
+import Flex from "../Shared/Flex";
+import Link from "../Shared/Link";
+import UserAvatar from "../Users/UserAvatar";
 
 interface Props {
   action: ProposalActionFragment;
@@ -47,10 +52,67 @@ const ProposalAction = ({
     if (!role) {
       return <Typography>{t("errors.somethingWentWrong")}</Typography>;
     }
+
+    const { name, color, permissions, members } = role;
+
     return (
-      <Typography marginBottom={3.5}>
-        {t("proposals.labels.proposedRole")}: {role.name}
-      </Typography>
+      <Box
+        marginBottom={3.5}
+        sx={{
+          backgroundColor: "rgb(0, 0, 0, 0.1)",
+          borderRadius: 2,
+          padding: 1.5,
+        }}
+      >
+        <Flex marginBottom={1.5}>
+          <Typography marginRight="0.5ch">
+            {t("proposals.labels.proposedRole")}:
+          </Typography>
+          <Circle
+            sx={{
+              color,
+              fontSize: 16,
+              marginRight: "0.5ch",
+              marginTop: 0.5,
+            }}
+          />
+          {name}
+        </Flex>
+
+        <Flex justifyContent="space-between">
+          <Box width="50%">
+            <Typography gutterBottom>
+              {t("permissions.labels.permissions")}
+            </Typography>
+
+            {permissions?.map((permission) => (
+              <Typography key={permission.id}>{permission.name}</Typography>
+            ))}
+          </Box>
+
+          <Divider orientation="vertical" flexItem sx={{ marginX: 3 }} />
+
+          <Box width="50%">
+            <Typography gutterBottom>{t("roles.labels.members")}</Typography>
+
+            {members?.map(({ user }) => (
+              <Link href={getUserProfilePath(user.name)} key={user.id}>
+                <Flex>
+                  <UserAvatar
+                    size={16}
+                    user={user}
+                    sx={{
+                      marginRight: 1,
+                      marginTop: 0.5,
+                    }}
+                  />
+                  <Typography color="primary">{user.name}</Typography>
+                </Flex>
+              </Link>
+            ))}
+          </Box>
+        </Flex>
+      </Box>
     );
   }
 
