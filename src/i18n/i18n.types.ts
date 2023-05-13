5848fc225406a2d0e709fea495c1ff0edb6bf8f9
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-05-13 16:47:31 -0400

    refactor: remove unused post list component

--- src/i18n/i18n.types.ts
+++ src/i18n/i18n.types.ts
@@ -1,12 +1,10 @@
 import "react-i18next";
 import en from "./locales/en.json";
 
-export type EnJson = typeof en;
-
 declare module "react-i18next" {
   interface CustomTypeOptions {
     resources: {
-      ns1: EnJson;
+      ns1: typeof en;
     };
   }
 }
