4ed50903108490545fd785b7e652d146364d3e63
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-06-14 21:53:19 -0400

    feat: use bold font for CardFooterButton

--- src/components/Shared/CardFooterButton.tsx
+++ src/components/Shared/CardFooterButton.tsx
@@ -3,6 +3,7 @@ import { styled } from "@mui/material/styles";
 
 const CardFooterButton = styled(Button)(() => ({
   borderRadius: 9999,
+  fontFamily: "Inter Bold",
   paddingLeft: 13,
   paddingRight: 13,
   textTransform: "none",
