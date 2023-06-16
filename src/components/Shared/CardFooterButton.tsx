904942c6b86c0285bfc0afc1f5a2d7797d339067
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-06-15 21:54:12 -0400

    feat: use sharper borders for left nav and CardFooterButton

--- src/components/Shared/CardFooterButton.tsx
+++ src/components/Shared/CardFooterButton.tsx
@@ -2,7 +2,6 @@ import { Button } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
 const CardFooterButton = styled(Button)(() => ({
-  borderRadius: 9999,
   fontFamily: "Inter Bold",
   paddingLeft: 13,
   paddingRight: 13,
