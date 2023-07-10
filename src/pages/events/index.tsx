c00f15cbac0d6fbd7b1b484a1e6066a2870bccc6
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-07-09 21:45:44 -0400

    feat: add wip tabs for events page

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -1,14 +1,29 @@
-// TODO: Add basic functionality for events - below is a WIP
+// TODO: Add remaining tabs functionality
 
-import { Typography } from "@mui/material";
+import {
+  Card,
+  CardContent as MuiCardContent,
+  styled,
+  Tab,
+  Tabs,
+  Typography,
+} from "@mui/material";
 import { NextPage } from "next";
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useEventsQuery } from "../../apollo/gen";
 import Event from "../../components/Events/Event";
 import LevelOneHeading from "../../components/Shared/LevelOneHeading";
 import ProgressBar from "../../components/Shared/ProgressBar";
 
+const CardContent = styled(MuiCardContent)(() => ({
+  "&:last-child": {
+    paddingBottom: 16,
+  },
+}));
+
 const EventsIndex: NextPage = () => {
+  const [tab, setTab] = useState(0);
   const { data, loading, error } = useEventsQuery();
   const { t } = useTranslation();
 
@@ -26,19 +41,34 @@ const EventsIndex: NextPage = () => {
 
   const { events } = data;
 
+  const handleTabChange = (_: any, value: number) => setTab(value);
+
   return (
     <>
       <LevelOneHeading header>
         {t("events.headers.discoverEvents")}
       </LevelOneHeading>
 
-      {events.map((event, index) => (
-        <Event
-          key={event.id}
-          event={event}
-          isLast={index + 1 === events.length}
-        />
-      ))}
+      <Card>
+        <Tabs textColor="inherit" value={tab} onChange={handleTabChange}>
+          <Tab label={t("events.labels.upcoming")} />
+          <Tab label={t("events.labels.thisWeek")} />
+          <Tab label={t("events.labels.online")} />
+          <Tab label={t("events.labels.past")} />
+        </Tabs>
+      </Card>
+
+      <Card>
+        <CardContent>
+          {events.map((event, index) => (
+            <Event
+              key={event.id}
+              event={event}
+              isLast={index + 1 === events.length}
+            />
+          ))}
+        </CardContent>
+      </Card>
     </>
   );
 };
