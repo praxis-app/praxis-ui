25ce416999180974f726b719fc4433486a8b0158
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-08-13 13:40:26 -0400

    feat: add groups section in docs

--- src/pages/docs/index.tsx
+++ src/pages/docs/index.tsx
@@ -4,7 +4,7 @@ import DocsSubheading from "../../components/Docs/DocsSubheading";
 import LevelOneHeading from "../../components/Shared/LevelOneHeading";
 
 const Docs: NextPage = () => (
-  <Box marginBottom={10}>
+  <Box marginBottom={15}>
     <LevelOneHeading header>What is Praxis?</LevelOneHeading>
 
     <Typography marginBottom={3}>
@@ -31,14 +31,29 @@ const Docs: NextPage = () => (
       At the core of Praxis are proposals and voting. Proposals are the basic
       unit of decision making in Praxis. Proposals can be created by any member
       of a group and can be voted on by any group member. Proposals can be used
-      to make decisions about virtually anything the group wants to decide on.
+      to make decisions about anything the group wants to decide on.
     </Typography>
 
-    <Typography marginBottom={1.5}>
+    <Typography marginBottom={3}>
       Once a proposal reaches enough votes to pass, its immediately marked as
       ratified. Depending on the proposal type, proposals will automatically
       implement themselves upon ratification. Roles and permissions changes, for
       instance, are implemented as soon as their respective proposal passes.
+    </Typography>
+
+    <DocsSubheading>Groups</DocsSubheading>
+
+    <Typography marginBottom={1.5}>
+      Almost everything in Praxis is scoped to groups, including both proposals
+      and events. Groups can be public or private, and can be used to organize a
+      community, a project, or anything else.
+    </Typography>
+
+    <Typography marginBottom={1.5}>
+      Every group has its own set of roles and permissions, as well as other
+      settings that can all be configured directly by group members via
+      proposals and voting, or by the groups admins. Its up to every group to
+      decide the best way to organize themselves.
     </Typography>
   </Box>
 );
