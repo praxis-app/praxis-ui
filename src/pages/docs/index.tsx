2b7c0086c60ae77f32ead8ae521c366dd837fe63
forrestwilkins <ethanforrestwilkins@gmail.com>
2023-08-13 10:18:36 -0400

    feat: add subheadings to docs page, update main heading

--- src/pages/docs/index.tsx
+++ src/pages/docs/index.tsx
@@ -4,17 +4,30 @@ import LevelOneHeading from "../../components/Shared/LevelOneHeading";
 
 const Docs: NextPage = () => (
   <>
-    <LevelOneHeading header>Overview</LevelOneHeading>
+    <LevelOneHeading header>What is Praxis?</LevelOneHeading>
+
+    <Typography marginBottom={3}>
+      Welcome to the Praxis documentation! This page will give you a brief
+      overview of Praxis and how to use it.
+    </Typography>
+
+    <Typography variant="h5" marginBottom={1.5}>
+      Overview
+    </Typography>
 
     <Typography marginBottom={1.5}>
       Praxis a social networking platform with features for collaborative
       decision making.
     </Typography>
 
-    <Typography marginBottom={1.5}>
+    <Typography marginBottom={3}>
       Unlike most social networks, Praxis is specifically designed to facilitate
       collective decision making and the building of social structures that make
       sense for the communities and organizations that use it.
+    </Typography>
+
+    <Typography variant="h5" marginBottom={1.5}>
+      Proposals
     </Typography>
 
     <Typography marginBottom={1.5}>
